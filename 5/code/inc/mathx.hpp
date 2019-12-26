2b0e2265810528b643ffc17aef8bc0d5c2fb69c3
m.mohinipande <m.mohinipande@gmail.com>
2019-12-26 19:19:49 +0530

    restructuring the directory

--- 5/code/inc/mathx.hpp
+++
@@ -1,9 +0,0 @@
-
-
-class math
-{
-    public:
-
-    math();
-    int multiplication(int a, int b);
-};
--- 5/library/src/mathx.cpp
+++
@@ -1,9 +0,0 @@
-#include "mathx.hpp"
-
-math::math()
-{
-}
-int math::multiplication(int a, int b)
-{
-    return a*b;
-}

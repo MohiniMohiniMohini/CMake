d8adab3a0c1b8c72f0223a7bda054a51a6551d07
m.mohinipande <m.mohinipande@gmail.com>
2019-12-26 15:33:28 +0530

    UI improvements

--- 1/main.cpp
+++ 1/main.cpp
@@ -5,7 +5,9 @@ int main()
 {   
     int a = 0;
     int b = 0;
+    cout << "Enter first number for multiplication: " << endl;
     cin >> a;
+    cout << "Enter second number for multiplication: " << endl;
     cin >> b;
 
     cout<<"Multiplication is: " << a << "*" << b << " = " << a*b << endl;
